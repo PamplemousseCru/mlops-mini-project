e198558dd401ae74945a88ebae9c31cd795e6679
Matthias Laithier <matthias.laithier@epita.fr>
2025-09-08 15:36:01 +0200

    feat: llm endpoint

--- 3_app.py
+++ 3_app.py
@@ -1,8 +1,14 @@
 import joblib
+import torch
 from fastapi import FastAPI
 from pydantic import BaseModel
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 model = joblib.load("regression.joblib")
+
+model_name = "t5-small"
+tokenizer = T5Tokenizer.from_pretrained(model_name)
+text_model = T5ForConditionalGeneration.from_pretrained(model_name)
 
 app = FastAPI()
 
@@ -13,7 +19,19 @@ class Numbers(BaseModel):
     garden: int
 
 
+class TextInput(BaseModel):
+    text: str
+
+
 @app.post("/predict")
 async def model_predict(numbers: Numbers):
     result = model.predict([[numbers.size, numbers.bedrooms, numbers.garden]])
     return {"y_pred": result[0]}
+
+
+@app.post("/llm")
+async def text_model_predict(input: TextInput):
+    input_ids = tokenizer.encode(input.text, return_tensors="pt")
+    outputs = text_model.generate(input_ids)
+    decoded_output = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return {"prediction": decoded_output}
--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.11-slim
 
 WORKDIR /app
 
-RUN pip install --no-cache-dir fastapi uvicorn joblib scikit-learn
+RUN pip install --no-cache-dir fastapi uvicorn joblib scikit-learn transformers torch
 
 COPY 3_app.py .
 COPY regression.joblib .
